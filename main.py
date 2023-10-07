deb270f9a29835801ca600fbb3c7b7373cdcd644
Pablo Escobar <esc20936@uvg.edu.gt>
2023-10-07 10:58:14 -0600

    feat(): image scan

--- main.py
+++ main.py
@@ -1,38 +1,48 @@
 from PIL import Image 
+from notas import notefreqs
 
-input_image = Image.open("cons.jpg") 
+def getNote(original_number):
+    normalized_number = original_number / 255
+    new_number = round(normalized_number * 15)
+    notes = list(notefreqs.keys())
+    return notes[new_number]
 
-pixel_map = input_image.load() 
-
-width, height = input_image.size 
-
-pixels = []
-
-for i in range(width//2): 
-	for j in range(height): 
-		r, g, b= input_image.getpixel((i, j)) 
-		pixels.append((r, g, b))
-
+def convertColorToRange(value,low=1,upper=100):
+    return (value * (upper - low) / 255) + low
+    
 
 
 def convertPixelsArrayToNotes(pixels):
     notes = []
     for pixel in pixels:
+        # r - frequency
+        # g - pitch
+        # b - amplitude
         r, g, b = pixel
-        if r == 0 and g == 0 and b == 0:
-            notes.append("C")
-        elif r == 0 and g == 0 and b == 255:
-            notes.append("D")
-        elif r == 0 and g == 255 and b == 0:
-            notes.append("E")
-        elif r == 0 and g == 255 and b == 255:
-            notes.append("F")
-        elif r == 255 and g == 0 and b == 0:
-            notes.append("G")
-        elif r == 255 and g == 0 and b == 255:
-            notes.append("A")
-        elif r == 255 and g == 255 and b == 0:
-            notes.append("B")
-        elif r == 255 and g == 255 and b == 255:
-            notes.append("C")
+        note = getNote(r)
+        g = convertColorToRange(g, 1, 20)
+        b = convertColorToRange(b, 0.5,1.5)
+        notes.append((note, g, b))
     return notes
+
+# tiene que recibir un objetoImgen de PIL
+def getNotesFromImage(input_image):
+    pixel_map = input_image.load() 
+    width, height = input_image.size 
+    pixels = []
+    for i in range(width//2): 
+        for j in range(height): 
+            r, g, b= input_image.getpixel((i, j)) 
+            pixels.append((r, g, b))
+
+    all_notes = convertPixelsArrayToNotes(pixels)
+
+    # separate notes in 3 arrays of the same size
+    notes1 = all_notes[:len(all_notes)//3]
+    notes2 = all_notes[len(all_notes)//3:2*len(all_notes)//3]
+    notes3 = all_notes[2*len(all_notes)//3:]
+
+    return [notes1, notes2, notes3]
+
+image = Image.open('cons.jpg') 
+notes = getNotesFromImage(image)
